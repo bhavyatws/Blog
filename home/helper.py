809b769ec36010182218b382c4995eb6e8119c8c
dalbeertws <dalbeer.tws@tekkiwebsolutions.com>
2022-02-16 16:15:57 +0530

    in mail sender ,heroku link has been passed so that it can work on heroku  deployed app

--- home/helper.py
+++ home/helper.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.core.mail import send_mail
 def send_mail_registration(email,token):
     subject='Your account need to be verified to login'
-    message=f'Paste the link to verify your account http://127.0.0.1:8000/verify/{token}'
+    message=f'Paste the link to verify your account https://shahidblogapp.herokuapp.com/verify/{token}'
     email_from=settings.EMAIL_HOST_USER
     recipient=[email]
     send_mail(subject,message,email_from,recipient)
